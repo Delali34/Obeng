f5d4df28a846f19e793f810dbb5190fd059a17dc
Delali34 <ernestanyomitse752@gmail.com>
2025-02-24 21:41:29 +0000

    update

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export const metadata = {
       "Transforming brands into digital success stories through strategic social media management and creative storytelling",
     images: [
       {
-        url: "/obeng.jpeg",
+        url: "https://opengraph.b-cdn.net/production/images/ac6569af-d637-4ddc-ad86-53cd2900c369.jpg?token=4LME-rLlV_bLBxJn6jz9PvNTwV3WJt3xEZLYovQH3sE&height=1079&width=1080&expires=33276433658",
         width: 1200,
         height: 630,
         alt: "Afia Tiwaa Obeng",
@@ -35,7 +35,9 @@ export const metadata = {
     title: "Afia Tiwaa Obeng",
     description:
       "Transforming brands into digital success stories through strategic social media management and creative storytelling",
-    images: ["/obeng.jpeg"],
+    images: [
+      "https://opengraph.b-cdn.net/production/images/ac6569af-d637-4ddc-ad86-53cd2900c369.jpg?token=4LME-rLlV_bLBxJn6jz9PvNTwV3WJt3xEZLYovQH3sE&height=1079&width=1080&expires=33276433658",
+    ],
     creator: "@yourusername",
   },
 };
