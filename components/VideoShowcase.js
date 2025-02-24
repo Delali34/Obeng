460a674795071b1c2c1cce9b88e117fd47f350f3
Delali34 <ernestanyomitse752@gmail.com>
2025-02-24 01:17:45 +0000

    deply

--- components/VideoShowcase.js
+++ components/VideoShowcase.js
@@ -246,7 +246,7 @@ const VideoShowcase = () => {
           </p>
         </motion.div>
 
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
           {videos.map((video) => (
             <VideoCard key={video.id} video={video} />
           ))}
